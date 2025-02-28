8231b21d5ac5a729a04a9e7eb2a26e1f9345a15c
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-28 11:34:04 +0900

    feat: JPQL 추가~~~~~~

--- src/main/java/org/ktb/springpost/repository/MemberRepository.java
+++ src/main/java/org/ktb/springpost/repository/MemberRepository.java
@@ -2,12 +2,18 @@ package org.ktb.springpost.repository;
 
 import org.ktb.springpost.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // Login
     Member findByEmailAndPassword(String email, String password);
+
+    // Login을 JPQL로 작성도 가능합니다
+    // JPQL은 객체 지향 쿼리라고 생각해주시면 됩니다.
+    @Query("SELECT m FROM Member m WHERE m.email = :email AND m.password = :password")
+    Member login(String email, String password);
 
     // 이메일을 통한 단건 조회
     Member findByEmail(String email);
