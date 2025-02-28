5a0d494492dfa82821ccfc31e8c7556e59de25e7
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-28 11:43:42 +0900

    fix: 악 JPQL 오랜만에 써서 잘못 작성함~

--- src/main/java/org/ktb/springpost/repository/MemberRepository.java
+++ src/main/java/org/ktb/springpost/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package org.ktb.springpost.repository;
 import org.ktb.springpost.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // Login을 JPQL로 작성도 가능합니다
     // JPQL은 객체 지향 쿼리라고 생각해주시면 됩니다.
     @Query("SELECT m FROM Member m WHERE m.email = :email AND m.password = :password")
-    Member login(String email, String password);
+    Member login(@Param("email") String email, @Param("password") String password);
 
     // 이메일을 통한 단건 조회
     Member findByEmail(String email);
